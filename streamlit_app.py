2e3f2f1b0f5f3a4a4ee5b5edf5deecd090ae1669
zoe-hoo <zoe.hu@merkle.com>
2026-09-08 15:17:13 +0800

    Clean up smoothie order submission code
    Removed unnecessary import and API call for smoothie data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,9 +46,6 @@ if ingredients_list:
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
+      
         st.success('Your Smoothie is ordered!', icon="✅")
-
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
         
