796df2b6959a9b6f05b348d2e64447850c12d3aa
zoe-hoo <zoe.hu@merkle.com>
2026-09-08 14:16:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import tempfile
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -11,10 +12,14 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-cnx = st.connection("snowflake")
+key_content = st.secrets["connections"]["snowflake"]["private_key_content"]
+with tempfile.NamedTemporaryFile(delete=False, suffix=".p8") as f:
+    f.write(key_content.encode())
+    key_path = f.name
+
+cnx = st.connection("snowflake", private_key_file=key_path)
 session = cnx.session()
 
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
