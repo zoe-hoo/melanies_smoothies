93e29f6d3c4cb3942eb7945c42f392ffd80fa6d0
zoe-hoo <zoe.hu@merkle.com>
2026-09-17 15:29:42 +0800

    Fetch and display watermelon nutrition information
    Added a new section to fetch and display nutrition information for watermelon from the Smoothiefroot API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,6 @@ if ingredients_list:
       
 #New section to display smoothiefroot nutrition information
 import requests  
-smoothiefroot_response = requests.get("(https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
         
