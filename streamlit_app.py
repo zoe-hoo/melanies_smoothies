e1e49304c9e7a0882130cec169f9c19ca2d20114
zoe-hoo <zoe.hu@merkle.com>
2026-09-17 15:52:04 +0800

    Display smoothiefroot nutrition as a dataframe
    Replace text display of smoothiefroot response with a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,6 @@ if ingredients_list:
 #New section to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
