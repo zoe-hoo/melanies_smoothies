b28572d8054f2cc665db6cc1787bd6403804adf3
zoe-hoo <zoe.hu@merkle.com>
2026-09-17 15:17:03 +0800

    Display smoothiefroot nutrition information as JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,5 +51,5 @@ if ingredients_list:
 #New section to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
         
